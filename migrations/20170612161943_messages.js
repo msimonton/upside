d9f8613d532d8fe64adaf69b5b3c07d4d2b27ae5
Marc Simonton <marcasimonton@gmail.com>
2017-06-13 00:01:22 -0600

    home route

--- migrations/20170612161943_messages.js
+++ migrations/20170612161943_messages.js
@@ -4,6 +4,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('messages', function(table) {
     table.increments();
     table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.string('name');
     table.string('email');
     table.bigInteger('phoneNumber');
     table.string('subject');
